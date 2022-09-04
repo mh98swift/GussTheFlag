4bbbb04811e6826b7add937ad4e941a3dfd573b4
mharari <mharari@varcode.com>
2022-09-04 11:05:16 +0300

    Initial Commit

---
+++ Day20GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day20GuessTheFlag
+//
+//  Created by VCM1 on 04/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day20GuessTheFlag/Day20GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day20GuessTheFlagApp.swift
+//  Day20GuessTheFlag
+//
+//  Created by VCM1 on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day20GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
